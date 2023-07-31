59de2c3fe4db46b461989e3c6650bb10ab170873
veenabudige <veenareddy442@gmail.com>
2023-07-31 15:50:32 +0530

    gitproject testing

---
+++ src/com/slokam/controller/ControllerTest.java
@@ -0,0 +1,12 @@
+package com.slokam.controller;
+
+public class ControllerTest {
+
+	public void test() {
+		System.out.println("Test1()");
+	}
+	
+	public static void main(String[] args) {
+		ControllerTest controller = new ControllerTest();
+	}
+}
